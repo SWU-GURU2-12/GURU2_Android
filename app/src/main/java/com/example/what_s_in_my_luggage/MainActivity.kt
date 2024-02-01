c78a67b889321e4194c46bb7e2426dd4fa7b40fb
ujin6666 <ujin736@gmail.com>
2024-02-01 13:57:06 +0900

    유저키 저장 + 각 activity로 전달

--- app/src/main/java/com/example/what_s_in_my_luggage/MainActivity.kt
+++ app/src/main/java/com/example/what_s_in_my_luggage/MainActivity.kt
@@ -10,6 +10,10 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var btnGetCarrying: ImageButton
+    lateinit var btnGetMyroom: ImageButton
+    lateinit var btnGetSomeones: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -31,26 +35,36 @@ class MainActivity : AppCompatActivity() {
             .replace(R.id.bottomNavigationFragment, bottomNavFragment)
             .commit()
 
-        val btnGetCarrying = findViewById<ImageButton>(R.id.btnGetCarrying)
-        val btnGetMyroom = findViewById<ImageButton>(R.id.btnGetMyroom)
-        val btnGetSomeones = findViewById<ImageButton>(R.id.btnGetSomeones)
+        btnGetCarrying = findViewById<ImageButton>(R.id.btnGetCarrying)
+        btnGetMyroom = findViewById<ImageButton>(R.id.btnGetMyroom)
+        btnGetSomeones = findViewById<ImageButton>(R.id.btnGetSomeones)
 
         // btnGetCarrying 버튼에 대한 클릭 리스너 설정
         btnGetCarrying.setOnClickListener {
-            val intent = Intent(this, PackLuggage::class.java)
-            startActivity(intent)
+            navigateToActivityWithUserKey(PackLuggage::class.java)
         }
 
         // btnGetMyroom 버튼에 대한 클릭 리스너 설정
         btnGetMyroom.setOnClickListener {
-            val intent = Intent(this, MyRoomActivity::class.java)
-            startActivity(intent)
+            navigateToActivityWithUserKey(MyRoomActivity::class.java)
         }
 
         // btnGetSomeones 버튼에 대한 클릭 리스너 설정
         btnGetSomeones.setOnClickListener {
-            val intent = Intent(this, CommunityActivity::class.java)
-            startActivity(intent)
+            navigateToActivityWithUserKey(CommunityActivity::class.java)
         }
+    }
+
+    private fun navigateToActivityWithUserKey(activityClass: Class<*>) {
+        val intent = Intent(this, activityClass)
+
+        // SharedPreferences에서 사용자 고유 키 값 가져오기
+        val sharedPrefs = getSharedPreferences(LoginActivity.PREFS_FILENAME, Context.MODE_PRIVATE)
+        val userKey = sharedPrefs.getString("USER_KEY", null)
+
+        // Intent에 사용자 키 값 추가
+        intent.putExtra("USER_KEY", userKey)
+
+        startActivity(intent)
     }
 }
