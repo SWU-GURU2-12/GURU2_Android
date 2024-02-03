1cac43db6b2cc3384e11e2f15f1064d7410bbbae
hye2021 <hyebeenbyeon@gmail.com>
2024-02-04 02:43:57 +0900

    fix packing frame activity build error

--- app/src/main/java/com/example/what_s_in_my_luggage/PackingFrameActivity.kt
+++ app/src/main/java/com/example/what_s_in_my_luggage/PackingFrameActivity.kt
@@ -1,5 +1,6 @@
 package com.example.what_s_in_my_luggage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -17,10 +18,6 @@ class PackingFrameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_packing_frame)
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-        }
 
         // 초기화
         val btnBack = findViewById<Button>(R.id.btnBack)
