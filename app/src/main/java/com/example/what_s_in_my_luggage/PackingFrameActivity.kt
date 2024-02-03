7adab56b60b5cf40894b57f1ac2f825149605c9e
ujin6666 <ujin736@gmail.com>
2024-02-04 01:54:53 +0900

    커뮤니티 레이아웃수정

--- app/src/main/java/com/example/what_s_in_my_luggage/PackingFrameActivity.kt
+++ app/src/main/java/com/example/what_s_in_my_luggage/PackingFrameActivity.kt
@@ -1,7 +1,9 @@
 package com.example.what_s_in_my_luggage
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
@@ -16,6 +18,10 @@ class PackingFrameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_packing_frame)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
 
         // 초기화
         val btnBack = findViewById<Button>(R.id.btnBack)
